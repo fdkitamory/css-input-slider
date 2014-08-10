0d5feeb0251fb29802f0b09d0fa1962a58f5b773
fdkitamory <fdk-work@yandex.ru>
2014-08-11 00:11:50 +0700

    more settings: on/off custom setting, on/off rotation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener( "DOMContentLoaded", function(){
 
 	var controlElements = document.querySelectorAll('.slider__switch'),
-		sliderSettingsInterval = (slider.setTimeInterval == null) ? 2000 : slider.setTimeInterval;
+		sliderSettingsInterval = (slider.customSettings == null) ? 2000 : slider.setTimeInterval;
 
 		// if (slider.setTimeInterval == null){
 		// 	console.log('Настройки не прописаны');
@@ -14,25 +14,19 @@ document.addEventListener( "DOMContentLoaded", function(){
 	  	for(var i=0; i<controlElements.length; i++) {
 	    	if (controlElements[i].checked == true ) return i;
 	  	}
-  		return -1;
 	}
 
 	function slideRotation () {
 		var activeSlide = findActiveSlide();
-
 		if (activeSlide >= controlElements.length - 1) {
 			controlElements[0].checked = true;
 		} else{
 			activeSlide++;
 			controlElements[activeSlide].checked = true;
 		};
-		
 	}
 
-	setInterval(
-		function() { 
-			console.log(sliderSettingsInterval);
-			slideRotation(); 
-		}, sliderSettingsInterval);
-
+	if (slider.rotation == null || slider.rotation == true) {
+		setInterval(function() { slideRotation(); }, sliderSettingsInterval);
+	}
 });
